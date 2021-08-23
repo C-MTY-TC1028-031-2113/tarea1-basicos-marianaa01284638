ebd69fd9460ed8558c169ec61ebe9810781aaa7d
Your Name <you@example.com>
2021-08-22 21:53:17 -0500

    marianaa01284638

--- assignments/02Peso/src/exercise.py
+++ assignments/02Peso/src/exercise.py
@@ -1,8 +1,9 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    pesoinicial = float(input("dame el peso inicial: "))
+    pesofinal = float(input("dame el peso final: "))
+    meses = float(input("dame la cantidad de meses: "))
+    bajar = (pesoinicial - pesofinal) / meses
+    print('lo que debes bajar por mes es: ' + str(bajar))
 
-
-
-if __name__ == '__main__':
-    main()
+main()
--- assignments/05CuentaBancaria/src/exercise.py
+++ assignments/05CuentaBancaria/src/exercise.py
@@ -1,8 +1,11 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
-
-
-
-if __name__ == '__main__':
-    main()
+    saldoanterior = float(input("dame el saldo del mes anterior: "))
+    ingresos = float(input("dame los ingresos: "))
+    egresos = float(input("dame los egresos: "))
+    cheques = float(input("dame el numero de cheques: "))
+    saldofinal = (saldoanterior + ingresos - egresos - (13 * cheques))
+    final = (saldofinal - (saldofinal * 0.075))
+    print('el saldo final de la cuenta es: ' + str(final))
+    
+main()
